bc44a6480765fe4a8c11ebac56cddba5ef6e8a2c
tina <christinachung2014@gmail.com>
2016-09-07 21:11:03 -0400

    Person1
    updated person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -30,16 +30,22 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	    String answer = new String();
+	    char[] arr = input.toCharArray();
+	    for (int i = 2; i < arr.length; i++){
+	        answer = answer + arr[i];
+	    }
+	    answer = answer + arr[0];
+	    answer = answer + arr[1];
+	  return answer;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
